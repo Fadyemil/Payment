67dceb6f28c7a58768d090f2226f378915bf0939
fady emil <fady46t45f@gmail.com>
2024-08-08 12:17:41 +0300

    add  repo

---
+++ lib/features/checkout/data/repos/checkout_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:payment/core/error/failures.dart';
+import 'package:payment/features/checkout/data/models/payment_intent_input_model.dart';
+
+abstract class CheckoutRepo {
+  Future<Either<Failure, void>> makePayment(
+      {required PaymentIntentInputModel paymentIntentInputModel});
+}
